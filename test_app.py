4df61c1be6b6aa6ee813dd8bd4329442ca279e5a
suryaprakashsahu <suryathe16@gmail.com>
2022-08-01 22:41:37 +0530

    Add pytest tests

--- test_app.py
+++ test_app.py
@@ -2,6 +2,8 @@ import os
 from fastapi.testclient import TestClient
 from .app.server import app
 
+import pytest
+
 os.chdir('app')
 client = TestClient(app)
 
@@ -25,7 +27,9 @@ def test_root():
     [TO BE IMPLEMENTED]
     Test the root ("/") endpoint, which just returns a {"Hello": "World"} json response
     """
-    pass
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
 
 
 def test_predict_empty():
@@ -33,7 +37,8 @@ def test_predict_empty():
     [TO BE IMPLEMENTED]
     Test the "/predict" endpoint, with an empty request body
     """
-    pass
+    response = client.post("/predict", json={})
+    assert response.status_code == 422
 
 
 def test_predict_en_lang():
@@ -41,7 +46,35 @@ def test_predict_en_lang():
     [TO BE IMPLEMENTED]
     Test the "/predict" endpoint, with an input text in English (you can use one of the test cases provided in README.md)
     """
-    pass
+    # Here event handlers (startup and shutdown) to run in your tests
+    # hence use TestClient with a `with` statement
+    # https://fastapi.tiangolo.com/advanced/testing-events/
+    with TestClient(app) as client:
+        response = client.post(
+            "/predict",
+            json={
+                "source": "Boston Globe",
+                "url": "http://www.boston.com/business/articles/2005/01/04/mike_wallace_subpoenaed?rss_id=BostonGlobe--BusinessNews",
+                "title": "Mike Wallace subpoenaed",
+                "description": "Richard Scrushy once sat down to talk with 60 Minutes correspondent Mike Wallace about allegations that Scrushy started a huge fraud while chief executive of rehabilitation giant HealthSouth Corp. Now, Scrushy wants Wallace to do the talking."
+                },
+        )
+        assert response.status_code == 200
+        assert response.json() == {
+            "scores": pytest.approx(
+                {
+                    "Business": 0.23508955472074963,
+                    "Entertainment": 0.5724817112974444,
+                    "Health": 0.0823957248580479,
+                    "Music Feeds": 0.003701017629065236,
+                    "Sci/Tech": 0.03923397760096245,
+                    "Software and Developement": 0.009537954019880498,
+                    "Sports": 0.051965575250882294,
+                    "Toons": 0.00559448462296766
+                }
+            ),
+            "label": "Entertainment",
+        }
 
 
 def test_predict_es_lang():
